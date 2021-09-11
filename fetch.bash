7637379b100055b4bbe0a5e3dfad12fb7fb1482d
tsubasaogawa <hurikake@gmail.com>
2021-09-11 19:42:51 +0900

    stop duplicate push

--- fetch.bash
+++ fetch.bash
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+set -eu
 
+# Get Binary
 curl -s -o ./embulk -L "https://dl.embulk.org/embulk-latest.jar"
-curl -i -s "https://dl.embulk.org/embulk-latest.jar" | grep -oP 'location: .+\.jar' | grep -oP '(\d+\.){1,2}(\d+)?' > /tmp/embulk.version
-md5sum /tmp/embulk | cut -f1 -d' ' > /tmp/embulk.md5
+
+# Save Metadata
+mkdir -p /tmp/artifacts
+VERSION=$(curl -i -s "https://dl.embulk.org/embulk-latest.jar" \
+  | grep -oP 'location: .+\.jar' \
+  | grep -oP '(\d+\.){1,2}(\d+)?' | tee /tmp/artifacts/embulk.version)
+MD5=$(md5sum /tmp/embulk | cut -f1 -d' ' | tee /tmp/artifacts/embulk.md5)
+
+cat << EOL
+  Version: ${VERSION}
+  Binary MD5: ${MD5}
+EOL
+
+# Exit if current version of embulk has been already built
+OWNER=$(cut -f1 -d'/' ${GITHUB_REPOSITORY})
+! curl -s 'https://registry.hub.docker.com/v2/repositories/${OWNER}/${IMAGE_NAME}/tags/' | jq -r '.results[].name' | grep $MD5
